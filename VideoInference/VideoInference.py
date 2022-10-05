8a961ee2bebe012753e417ae711f10a64154edee
Jaime Laster <jacob.laster23@gmail.com>
2022-10-05 11:56:49 -0400

    Created VideoInference class and started implementation

--- VideoInference/VideoInference.py
+++ VideoInference/VideoInference.py
@@ -1,1 +1,9 @@
+from ObjectRecognition import *
+from CameraDriver import LowLatencyCapture
 
+class VideoInference:
+    def __init__(self, connectionAddress: str) -> None:
+        self.llc = LowLatencyCapture(connectionAddress)
+    
+    def GetNextFrame():
+        pass
